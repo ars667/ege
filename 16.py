6daebcb9508380a925229ed37506d6ca6ba6b452
bombombomqweqwe <160270766+bombombomqweqwe@users.noreply.github.com>
2024-02-23 14:44:31 +0300

    Add files via upload

--- 16.py
+++ 16.py
@@ -1,10 +1,8 @@
-def f(n):
-    if n <= 3:
-        return n
-    if n > 3 and n % 3 == 0:
-        return n ** 3 + f(n - 1)
-    if n > 3 and n % 3 == 1:
-        return 4 + f(n//3)
-    if n > 3 and n % 3 == 2:
-        return n ** 2 + f(n - 2)
-print(f(100))
+def f(a, b):
+    if a > b:
+        return 0
+    if a == b:
+        return 1
+    if a < b:
+        return f(a + 1, b) + f(a + 2, b) + f(a * 4, b)
+print(f(1, 13))
